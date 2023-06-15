ba21f4ae306a65892e5358730b0c3bd0715b96a8
Anshul-Sangrame <anshul.sangrame@gmail.com>
2023-06-15 22:17:19 +0530

    Playing with react

---
+++ frontend/src/App.js
@@ -0,0 +1,40 @@
+import { Fragment, useState } from 'react';
+import './App.css';
+import { Link, Route, Routes } from 'react-router-dom';
+import { Auth } from './Components/Auth';
+
+function App() {
+  const getDeafaultValue = () => {
+    var item = localStorage.getItem('IsAuth');
+    if (item == 'true') return true;
+    return false;
+  }
+  const [IsAuth,setIsAuth] = useState(getDeafaultValue())
+  const Onclick = () => {
+    localStorage.setItem('IsAuth',!IsAuth);
+    setIsAuth(!IsAuth);
+  }
+  
+  return (
+    <Fragment>
+      <button onClick={() => {Onclick()}}>
+        {IsAuth? "Authenticated": "Not Authenticated"}
+      </button>
+      <Link to='/home'>Click</Link>
+      {/* Public routes */}
+      <Routes>
+        <Route path='' element={<h1>Login By Harsh</h1>} />
+        <Route path='signUp' element={<h1>SignUp By Muskan</h1>} />
+      </Routes>
+
+      {/* Private routes */}
+      <Auth isAuth={IsAuth}>
+        <Routes>
+          <Route path='home' element={<h1>Home</h1>} />
+        </Routes>
+      </Auth>
+    </Fragment>
+  );
+}
+
+export default App;
